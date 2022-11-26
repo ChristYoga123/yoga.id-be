3f1f800820edac6fc83fe344756b1f570a5354f6
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-26 13:47:00 +0700

    create project migration and model

---
+++ database/migrations/2022_11_26_062722_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('hero_image');
+            $table->longText('description');
+            $table->enum('category', ['Course', 'Job', 'Personal']);
+            $table->enum('status', ['On Going', 'Launched']);
+            $table->string('url', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
